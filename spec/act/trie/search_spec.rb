8f8e8066439880cca46c6811ddd65b443e59fa7f
Sergey Bezugliy <s.bezugliy@gmail.com>
2019-04-29 18:58:06 +0300

    Trie search spec

--- spec/act/trie/search_spec.rb
+++ spec/act/trie/search_spec.rb
@@ -1,13 +1,24 @@
 RSpec.describe ACT::Trie do
   # Trie for dict "a cba cbe de a dfe abe df x dfe", with indices as in array
   describe 'Search inside using trie' do
-    let!(:dict) { %w[he she her his him they their] }
+    let!(:dict) { %w[he she her his him he they their she] }
     let!(:text) { 'he their them height have then their shelter' }
     let!(:trie) { ACT::Trie.new(dict) }
+    let!(:result) {
+      [ {:word=>"he", :indexes=>[0, 5]},
+        {:word=>"their", :indexes=>[7]},
+        {:word=>"he", :indexes=>[0, 5]},
+        {:word=>"he", :indexes=>[0, 5]},
+        {:word=>"he", :indexes=>[0, 5]},
+        {:word=>"he", :indexes=>[0, 5]},
+        {:word=>"their", :indexes=>[7]},
+        {:word=>"he", :indexes=>[0, 5]},
+        {:word=>"she", :indexes=>[1, 8]},
+        {:word=>"he", :indexes=>[0, 5]}]
+    }
 
     it 'should parse text' do
-      p trie.parse(text)
-      expect(true).to be(true)
+      expect(trie.parse(text)).to eq(result)
     end
   end
 end
